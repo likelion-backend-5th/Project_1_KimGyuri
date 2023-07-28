359b1d275a482447a59b1a4718a237e9fa27f336
kkyuwoo <ggya5@naver.com>
2023-07-28 18:54:56 +0900

    feat: 댓글 조회 시 댓글 등록 사용자 아이디 정보 추가

--- src/main/java/com/example/MutsaMarket/dto/comment/CommentListDto.java
+++ src/main/java/com/example/MutsaMarket/dto/comment/CommentListDto.java
@@ -6,12 +6,14 @@ import lombok.Data;
 @Data
 public class CommentListDto {
     private Long id;
+    private String username;
     private String content;
     private String reply;
 
     public static CommentListDto fromEntity(CommentEntity entity) {
         CommentListDto dto = new CommentListDto();
         dto.setId(entity.getId());
+        dto.setUsername(entity.getUser().getUsername());
         dto.setContent(entity.getContent());
         dto.setReply(entity.getReply());
         return dto;
